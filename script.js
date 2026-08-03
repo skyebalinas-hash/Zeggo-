0a9ce9ce50ce3b55e46e7d8f6b0af297aec97d0c
skyebalinas-hash <skyebalinas@gmail.com>
2026-08-03 20:42:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -244,6 +244,8 @@ function filterProperties() {
 
     displayProperties(filtered);
 
+   }
+
 /* ==========================================
    SEARCH SUGGESTIONS
 ========================================== */
